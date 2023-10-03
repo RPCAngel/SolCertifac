fa454105f9a260524a436f05f100d81e9e0beb63
AngelCRP14 <132794698+AngelCRP14@users.noreply.github.com>
2023-10-02 19:54:14 -0600

    Agregar archivos de proyecto.

---
+++ Certifac/Controllers/HomeController.cs
@@ -0,0 +1,92 @@
+﻿using Certifac.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.MobileControls;
+
+namespace Certifac.Controllers
+{
+    public class HomeController : Controller
+    {
+        Cer_AddendasEntities db = new Cer_AddendasEntities();
+        public ActionResult Index()
+        {
+           
+            try
+            {
+                List<Addendas> ls = db.Addendas.ToList();
+                db.Dispose();
+                return View(ls);
+            }
+            catch (Exception ex)
+            {
+                TempData["e"] = ex.Message;
+                return View();
+            }
+           
+        }
+
+        public ActionResult Create()
+        {
+            
+                List<Addendas> ls = db.Addendas.ToList();
+                db.Dispose();
+                return View();
+            
+        }
+        [HttpPost]
+        public ActionResult Create(Addendas ad)
+        {
+            try
+            {
+                db.Addendas.Add(ad);
+                db.SaveChanges();
+                db.Dispose();
+
+                return RedirectToAction("index");
+            }
+            catch 
+            {
+
+                return View();
+            }
+        }
+        public ActionResult Edit(Guid id)
+        {
+            // Addendas ad = db.Addendas.Find(id);
+            
+           Addendas ad = db.Addendas.Where(x => x.IdAddenda == id).FirstOrDefault();
+            return View(ad);
+        }
+        [HttpPost]
+        public ActionResult Edit(Addendas ad)
+        {
+            try
+            {
+                Addendas adn = db.Addendas.Find(ad.IdAddenda);
+                adn.NombreAddenda = ad.NombreAddenda;
+                adn.XML = ad.XML;
+                adn.FechaModificacion = ad.FechaModificacion;
+                adn.Usuario = ad.Usuario;
+                adn.Estado = ad.Estado;
+
+                db.SaveChanges();
+                db.Dispose();
+
+                return RedirectToAction("index");
+
+            }
+            catch 
+            {
+                return View();
+            }
+        }
+
+        
+       
+
+    }
+}
